405eb36b4dd43b22505e9932191210368c0598df
idelairre <idelairre@gmail.com>
2015-09-09 19:31:36 -0400

    serializer now returns title url

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :subheading, :user_id, :created_at, :updated_at
+  attributes :id, :title, :title_url, :body, :subheading, :user_id, :created_at, :updated_at
 
   belongs_to :user
 end
