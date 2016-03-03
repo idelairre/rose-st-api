d88579dc18971bbf91b129e5bd533f1273c90bda
idelairre <idelairre@gmail.com>
2016-03-03 06:12:24 -0500

    conditionally loads env file based on environment

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -1,10 +1,12 @@
 module EnvironmentVariables
   class Application < Rails::Application
-    config.before_configuration do
-      env_file = Rails.root.join("config", 'env.yml').to_s
-      if File.exists?(env_file)
-        YAML.load_file(env_file)[Rails.env].each do |key, value|
-          ENV[key.to_s] = value
+    if Rails.env == 'development' || Rails.env == 'test'
+      config.before_configuration do
+        env_file = Rails.root.join("config", 'env.yml').to_s
+        if File.exists?(env_file)
+          YAML.load_file(env_file)[Rails.env].each do |key, value|
+            ENV[key.to_s] = value
+          end
         end
       end
     end
